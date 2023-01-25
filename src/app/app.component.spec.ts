19a1c48505525b8fa7ce5fcaba4031833c883e4f
Arya Indra Tanaya <aryaindratanaya@gmail.com>
2023-01-25 22:32:13 +0800

    Remove unnecessary boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,18 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'my-dashboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('my-dashboard')
-  })
-
-  it('should render title', () => {
+  it('should render the app title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'my-dashboard app is running!',
-    )
+    expect(compiled.querySelector('h1')?.textContent).toContain('My Dashboard!')
   })
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-dashboard'
-}
+export class AppComponent {}
